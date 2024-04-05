18b4315ce43bc4ad199804949060ea2940bb8971
hchen1267 <107211590+hchen1267@users.noreply.github.com>
2024-04-05 14:53:44 -0400

    Changed difficulty to be php file

---
+++ difficulty.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="css/difficulty.css" rel="stylesheet">
+</head>
+<body>
+  <div><a href = "login.php"><img src = "css/img/back_button.png" class="back_button"></a></div>
+  <div class="content">
+    <h2>Choose Your Difficulty Level</h2>
+
+    <div class="buttons">
+      <form action="gamepage.php" method="post">
+      <button type="submit" name="difficulty" value="Easy" class="easy_button">Easy</button>
+      <button type="submit" name="difficulty" value="Medium" class="medium_button">Medium</button>
+      <button type="submit" name="difficulty" value="Hard" class="hard_button">Difficult</button>
+      </form>
+    </div>
+    
+  </div>
+  
+</body>
+</html>
